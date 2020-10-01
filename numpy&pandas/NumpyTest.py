87b10602f98be3806c69a3dd1817a47df84add46
Aye10032 <yeyu6_505@126.com>
2020-10-02 00:22:18 +0800

    NumpyTest

--- numpy&pandas/NumpyTest.py
+++ numpy&pandas/NumpyTest.py
@@ -7,3 +7,26 @@ print(array)
 print('number of dim:', array.ndim)
 print('shape:', array.shape)
 print('size:', array.size)
+
+print('----------------------------------')
+
+array = np.array([2, 23, 4], dtype=np.int64)
+print(aray.dtype)
+array = np.array([2, 23, 4], dtype=np.float32)
+print(array.dtype)
+print('----------------------------------')
+
+array = np.zeros((3, 4), dtype=int)
+print(array)
+print('----------------------------------')
+
+array = np.arange(10, 20, 2)
+print(array)
+array = np.arange(12).reshape((3, 4))
+print(array)
+print('----------------------------------')
+
+array = np.linspace(1, 10, 5)
+print(array)
+array = np.linspace(1, 10, 9).reshape((3, 3))
+print(array)
